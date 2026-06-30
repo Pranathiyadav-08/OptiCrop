ddb416d2bcee27ee0d3c48e59750915805a4e86c
K MALIK BASHA <kotturumalik@gmail.com>
2026-06-30 11:21:59 +0530

    Added OptiCrop project

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ df = load_and_process()
 
 print("\nSeasonal Crop Info:")
 print(df.groupby('season')['label'].unique())
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Season count plot
+sns.countplot(x='season', data=df)
+plt.title("Season Distribution")
+plt.show()
